e3a6826fcaf4358952474c87def9a0ccbbe0afd3
JKKIM <jkesonjk@gmail.com>
2023-07-12 02:00:22 +0900

    feat: 상세 페이지 생성 및 데이터 로드 로직 구현

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { IIssue } from 'types';
 
-type Props = {};
+const githubToken = process.env.REACT_APP_GITHUB_TOKEN;
 
-const Detail = (props: Props) => {
+const Detail = () => {
+  const [issueDetail, setIssueDetail] = useState<IIssue>();
+
+  const params = useParams();
+  const { issueNumber } = params;
+
+  useEffect(() => {
+    const getDetailIssue = async () => {
+      const response = await fetch(`https://api.github.com/repos/facebook/react/issues/${issueNumber}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${githubToken}`,
+        },
+      });
+
+      const detailIssue = await response.json();
+
+      setIssueDetail(detailIssue);
+    };
+
+    getDetailIssue();
+  }, []);
+  console.log(issueDetail);
   return <div>Detail</div>;
 };
 
