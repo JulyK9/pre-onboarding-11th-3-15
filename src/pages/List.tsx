ec38039244cb33667ad51a3a58d35f7040053bfd
JKKIM <jkesonjk@gmail.com>
2023-07-12 16:01:36 +0900

    context를 통한 이슈 리스트 불러오기 적용 수정

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,40 +1,17 @@
 import Item from 'components/common/Item';
-import { useEffect, useState } from 'react';
+import { useGetIssue } from 'context';
 import { IIssue } from 'types';
 
-const githubToken = process.env.REACT_APP_GITHUB_TOKEN;
-
 const List = () => {
-  const [issues, setIssues] = useState<IIssue[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-
-    const getIssueList = async () => {
-      const response = await fetch('https://api.github.com/repos/facebook/react/issues', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${githubToken}`,
-        },
-      });
-
-      const issueList = await response.json();
-
-      setIssues((prev) => [...prev, ...issueList]);
-      setIsLoading(false);
-    };
-
-    getIssueList();
-  }, []);
-
+  const { issueList, isLoading }: any = useGetIssue();
+  console.log('issueList: ', issueList);
+  // console.log('isLoading: ', isLoading);
   return (
     <>
       {isLoading ? (
         <h2>Loading...</h2>
       ) : (
-        <ul>{issues?.map((issue: IIssue) => <Item key={issue?.id} issue={issue} />)}</ul>
+        <ul>{issueList?.map((issue: IIssue) => <Item key={issue?.id} issue={issue} />)}</ul>
       )}
     </>
   );
