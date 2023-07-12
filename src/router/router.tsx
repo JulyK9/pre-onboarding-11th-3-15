a2f3e17d7ce3ec7aa8d145102e2b8a267a38a2ad
JKKIM <jkesonjk@gmail.com>
2023-07-12 09:42:23 +0900

    feat: 라우팅 에러 설정 및 컴포넌트 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import List from 'pages/List';
-import Layout from 'components/common/Layout';
 import Detail from 'pages/Detail';
+import Layout from 'components/common/Layout';
+import Error from 'components/common/Error';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <Error />,
     children: [
       {
         path: 'issues',
